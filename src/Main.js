9642992cffb1176c2d09d5dc92c47a96de28fb84
ShayBox <shayneehartford@gmail.com>
2017-09-25 07:37:50 -0400

    Added Main

---
+++ src/Main.js
@@ -0,0 +1,29 @@
+const { readdirSync, statSync } = require('fs');
+const { spawn } = require('child_process');
+const { join } = require('path');
+
+isDirectory(join('.', './src/Bots')).forEach(dir => {
+  start(`src/Bots/${dir}`);
+});
+
+function isDirectory(source) {
+  return readdirSync(source).filter(name => statSync(`${source}/${name}`).isDirectory());
+}
+
+function start(input) {
+  const proc = spawn('node', ['../../__Global/Main.js'], { cwd: input });
+
+  proc.stdout.on('data', data => {
+    console.log(String(data));
+  });
+
+  proc.stderr.on('data', data => {
+    console.error(String(data));
+  });
+
+  proc.on('close', () => {
+    setTimeout(() => {
+      start(input);
+    }, 1000 * 5);
+  });
+}

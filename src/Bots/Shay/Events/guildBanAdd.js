e545062ba3602ed09fa018e45372bb9b50c05abc
ShayBox <shayneehartford@gmail.com>
2017-09-30 11:15:08 -0400

    Added Mod Log

--- src/Bots/Shay/Events/guildBanAdd.js
+++ src/Bots/Shay/Events/guildBanAdd.js
@@ -2,14 +2,14 @@ const Events = require(`../../../__Global/Structures/Events`);
 const { MessageEmbed } = require(`discord.js`);
 
 class Event extends Events {
-  run(client, member) {
+  run(client, guild, user) {
     if (process.env.LOCAL) return;
-    if (member.guild.id !== `361532026354139156`) return;
-    client.fetchWebhook(process.env.WEBHOOK_MemberLog_ID, process.env.WEBHOOK_MemberLog_TOKEN).then(webhook => {
+    if (guild.id !== `361532026354139156`) return;
+    client.fetchWebhook(process.env.WEBHOOK_ModLog_ID, process.env.WEBHOOK_ModLog_TOKEN).then(webhook => {
       const embed = new MessageEmbed()
-        .setAuthor(member.user.username, member.user.displayAvatarURL())
+        .setAuthor(user.username, user.displayAvatarURL())
         .setColor(0x00FF00)
-        .setFooter(`Joined`)
+        .setFooter(`Banned`)
         .setTimestamp();
       webhook.send({ embed });
     });
--- src/Bots/Shay/Events/guildMemberRemove.js
+++ src/Bots/Shay/Events/guildMemberRemove.js
@@ -4,6 +4,7 @@ const { MessageEmbed } = require(`discord.js`);
 class Event extends Events {
   run(client, member) {
     if (process.env.LOCAL) return;
+    if (member.guild.id !== `361532026354139156`) return;
     client.fetchWebhook(process.env.WEBHOOK_MemberLog_ID, process.env.WEBHOOK_MemberLog_TOKEN).then(webhook => {
       const embed = new MessageEmbed()
         .setAuthor(member.user.username, member.user.displayAvatarURL())

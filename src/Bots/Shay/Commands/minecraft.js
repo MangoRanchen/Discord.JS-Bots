88c48fba833934c342e01e3d62b0317a8513e3cf
ShayBox <shayneehartford@gmail.com>
2017-10-01 18:11:56 -0400

    Fixed Minecraft command invalid name

--- src/Bots/Shay/Commands/minecraft.js
+++ src/Bots/Shay/Commands/minecraft.js
@@ -17,19 +17,21 @@ class Command extends Commands {
     });
   }
 
-  async run(client, message, args) {
+  run(client, message, args) {
     if (args.length < 1) return client.errorMessage(message, message.content.replace(client.botPrefix, ``), this.usage);
     if (args[0] < 3 || args[0] > 32) return client.errorMessage(message, null, `Please input a valid Name`);
 
-    const resolveName = await username(args[0]).catch(() => client.errorMessage(message, null, `Invalid Name or UUID`));
-
-    const embed = new MessageEmbed()
-      .setAuthor(`${resolveName.name} (UUID: ${resolveName.id})`, `https://visage.surgeplay.com/face/${resolveName.id}`)
-      .setImage(`https://visage.surgeplay.com/full/512/${resolveName.id}`)
-      .setColor(0x00FF00)
-      .setFooter(client.botName)
-      .setTimestamp();
-    client.send(message, { embed });
+    username(args[0]).then(data => {
+      const embed = new MessageEmbed()
+        .setAuthor(`${data.name} (UUID: ${data.id})`, `https://visage.surgeplay.com/face/${data.id}`)
+        .setImage(`https://visage.surgeplay.com/full/512/${data.id}`)
+        .setColor(0x00FF00)
+        .setFooter(client.botName)
+        .setTimestamp();
+      client.send(message, { embed });
+    }).catch(() => {
+      client.errorMessage(message, null, `Invalid Name, Please input a valid Name`);
+    });
   }
 }
 

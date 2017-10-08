a6180e3eb2b7ee76c4608c633790d66e2435882a
ShayBox <shayneehartford@gmail.com>
2017-10-08 16:07:40 -0400

    Converted Global guildCreate and guildDelete to use client.send

--- src/__Global/Events/guildDelete.js
+++ src/__Global/Events/guildDelete.js
@@ -10,7 +10,7 @@ class Event extends Events {
 			.setColor(0x00FF00)
 			.setFooter(`Left`)
 			.setTimestamp();
-		client.channels.get(`363003869288202242`).send({ embed });
+		client.send(client.channels.get(`363003869288202242`), { embed });
 	}
 }
 

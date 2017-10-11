84ae954bb0a776d71d9ae2913b13fde79c2e52ff
ShayBox <shayneehartford@gmail.com>
2017-10-11 18:44:56 -0400

    Added semi-working mongodb database functions

--- src/__Global/Structures/Database.js
+++ src/__Global/Structures/Database.js
@@ -1,6 +1,73 @@
+const { connect } = require(`mongodb`);
+
 class Database {
-	check() {
-		//
+	constructor() {
+		this.url = process.env.MONGODB_URI;
+	}
+
+	insert(objects) {
+		let value = null;
+		connect(this.url, (error, db) => {
+			if (error) return console.error(error);
+			console.log(`Connected successfully to server`);
+
+			var collection = db.collection(`documents`);
+			collection.insertMany(objects, (error, result) => {
+				if (error) return console.error(error);
+				console.log(`Inserted document(s) into the collection`);
+				value = result;
+			});
+		});
+		return value;
+	}
+
+	find(object) {
+		let value = null;
+		connect(this.url, (error, db) => {
+			if (error) return console.error(error);
+			console.log(`Connected successfully to server`);
+
+			var collection = db.collection(`documents`);
+			collection.find(object).toArray((error, docs) => {
+				if (error) return console.error(error);
+				console.log(`Found the following records`);
+				console.log(docs);
+				value = docs;
+			});
+		});
+		return value;
+	}
+
+	update(object, object2) {
+		let value = null;
+		connect(this.url, (error, db) => {
+			if (error) return console.error(error);
+			console.log(`Connected successfully to server`);
+
+			var collection = db.collection(`documents`);
+			collection.updateOne(object, { $set: object2 }, (error, result) => {
+				if (error) return console.error(error);
+				console.log(`Updated the document with the field a equal to 2`);
+				value = result;
+			});
+		});
+		return value;
+	}
+
+	remove(object) {
+		let value = null;
+		connect(this.url, (error, db) => {
+			if (error) return console.error(error);
+			console.log(`Connected successfully to server`);
+
+			var collection = db.collection(`documents`);
+			collection.deleteOne(object, (error, result) => {
+				if (error) return console.error(error);
+				console.log(`Removed the document with the field a equal to 3`);
+				value = result;
+			});
+		});
+		return value;
 	}
 }
 

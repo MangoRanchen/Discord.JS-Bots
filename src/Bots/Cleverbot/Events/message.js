3936cc1bd10019c11c678da35bfc84f390447c37
ShayBox <shayneehartford@gmail.com>
2017-10-12 19:55:57 -0400

    Increased cooldown to 2 seconds Added non alphanumeric filter

--- src/Bots/Cleverbot/Events/message.js
+++ src/Bots/Cleverbot/Events/message.js
@@ -5,21 +5,20 @@ cleverbot.configure({ botapi: process.env.CLEVERBOT_API });
 
 class Event extends Events {
 	run(client, message) {
-		if (message.channel.name.includes(`cleverbot`)) {
-			if (message.author.bot) return;
+		if (!message.channel.name.includes(`cleverbot`)) return;
+		if (message.author.bot) return;
+		if (!message.content.match(/[A-Z0-9]/i)) return;
+		if (client.checkCooldown(message.author.id)) return client.send(message, `Cooldown, please wait!`).then(m => m.delete({ timeout: 1000 }));
+		client.addCooldown(message.author.id, `2`);
 
-			if (client.checkCooldown(message.author.id)) return client.send(message, `Cooldown, please wait!`).then(m => m.delete({ timeout: 1000 }));
-			client.addCooldown(message.author.id, `1`);
-
-			message.channel.startTyping();
-			cleverbot.write(message.content, response => {
-				client.send(message, response.output);
-				message.channel.stopTyping();
-				client.database.find({ USED_API_CALLS: { $type: 16 } }).then(data => {
-					client.database.update({ USED_API_CALLS: { $type: 16 } }, { USED_API_CALLS: data[0].USED_API_CALLS + 1 });
-				});
+		message.channel.startTyping();
+		cleverbot.write(message.content, response => {
+			client.send(message, response.output);
+			message.channel.stopTyping();
+			client.database.find({ USED_API_CALLS: { $type: 16 } }).then(data => {
+				client.database.update({ USED_API_CALLS: { $type: 16 } }, { USED_API_CALLS: data[0].USED_API_CALLS + 1 });
 			});
-		}
+		});
 	}
 }
 
